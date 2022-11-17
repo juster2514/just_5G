fd445b6b6d933ca1595ccdfa6daaa3f6dc06bff2
juster2514 <2369850925@qq.com>
2022-11-17 16:22:12 +0800

    just_5g 1change

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -131,6 +131,7 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   # Include the install script for each subdirectory.
   include("/home/ljj/just_5g_ws/build/gtest/cmake_install.cmake")
+  include("/home/ljj/just_5g_ws/build/chassis_drive/cmake_install.cmake")
   include("/home/ljj/just_5g_ws/build/wit_imu/cmake_install.cmake")
 
 endif()
